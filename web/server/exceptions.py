dd01c5b6197159eca68d167b65e05e5c11cc85c0
MSLANDERS <mslanders0@gmail.com>
2025-04-09 23:10:30 +0530

    MSLANDERS

---
+++ web/server/exceptions.py
@@ -0,0 +1,8 @@
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+#Dont Remove My Credit @MSLANDERS 
+# For Any Kind Of Error Ask Us In Support Group @MSLANDERS_HELP
+
+class FIleNotFound(Exception):
+    message = "File not found"
