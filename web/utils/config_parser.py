1e21b3ad6e409524368d7edea4531c039f6db722
MSLANDERS <mslanders0@gmail.com>
2025-04-09 23:25:23 +0530

    MSLANDERS

--- web/utils/config_parser.py
+++ web/utils/config_parser.py
@@ -1,9 +1,8 @@
 from os import environ
 from typing import Dict, Optional
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+#Dont Remove My Credit @MSLANDERS 
+# For Any Kind Of Error Ask Us In Support Group @MSLANDERS_HELP
 
 class TokenParser:
     def __init__(self, config_file: Optional[str] = None):
@@ -21,6 +20,5 @@ class TokenParser:
         )
         return self.tokens
 
-#Dont Remove My Credit @AV_BOTz_UPDATE 
-#This Repo Is By @BOT_OWNER26 
-# For Any Kind Of Error Ask Us In Support Group @AV_SUPPORT_GROUP
+#Dont Remove My Credit @MSLANDERS 
+# For Any Kind Of Error Ask Us In Support Group @MSLANDERS_HELP
